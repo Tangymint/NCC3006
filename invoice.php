2cce283aa1e3166f9875efeb5dc4309d848ca1a9
Rohit Menon <rohitsm@gmail.com>
2014-04-09 02:55:54 +0800

    Added GUI elements to index.html and invoice.php

--- invoice.php
+++ invoice.php
@@ -1,32 +1,68 @@
-<?php
+<!--
+ 	Author: Rohit Menon
+	Matric No. U1222512H
+	Lab group: SSP3
+-->
+<html>
+
+	<head><title>Purchase Invoice</title></head>
+	<!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
 
 
-// Function to read input from file
-function readFromFile(){
+	<div class="container">
+		<div class="md-offset-3">
+			<h3 class="pull-center">Invoice</h3>
 
-	$file = fopen("order.txt", "r+") or exit("Unable to open order.txt file!");
+			<?php
+				echo "<h4>Customer Name: ".$_GET["name"]. "</h4>";
+			?>
+				<!-- <table border="border"> -->
+				<table class="table table-striped table-bordered">
+			<?php
+				echo "<thead>";
+					echo "<tr>";
+						echo "<td colspan=\"2\">Customer Name</td>";
+						echo "<td colspan=\"2\">".$_GET["name"]. "</td>";
+					echo "</tr>";
+				echo "</thead>";
 
-	$total_records = array( "apples" => "",
-						"oranges" => "",
-						"bananas" => "");
+			?>		
+					<!-- First row: Column heading -->
+					<tr>
+						<th>Item</th>
+						<th>Qty</th>
+						<th>Rate</th>
+						<th>Cost</th>
+					</tr>
 
-	foreach ($total_records as $fruit => $quantity) {
-		$line = fgets($file);
-		$quantity = explode(":", $line);
-		// var_dump($line);
-		echo "<br>";
-		var_dump($quantity[1]);
-		// printf($fruit. " : " .$line);
-		echo "<br>";
-	}	
-}
+				<tr>
+					<td>Apples</td>
+					<td> <?php echo $_GET["apples"] ?> </td>
+					<td> $0.69 </td>
+					<td> <?php echo number_format(($_GET["apples"] * 0.69), 2) ?> </td>
+				</tr>
 
+				<tr>
+					<td>Oranges</td>
+					<td> <?php echo $_GET["oranges"] ?> </td>
+					<td> $0.59 </td>
+					<td> <?php echo number_format(($_GET["oranges"] * 0.59), 2) ?> </td>
+				</tr>
 
-readFromFile();
+				<tr>
+					<td>Bananas</td>
+					<td> <?php echo $_GET["bananas"] ?> </td>
+					<td> $0.39 </td>
+					<td> <?php echo number_format(($_GET["bananas"] * 0.39), 2) ?> </td>
+				</tr>
 
+				
+				</table>
 
+				<h5>Payment method: <?php echo $_GET["payment"] ?> </h5>
+				<h5>Total Cost = <?php echo $_GET["total"] ?> </h5>
+		</div>
+	</div>
 
-
-
-
-?>
+</html>
--- payment.php
+++ payment.php
@@ -1,20 +1,129 @@
+<!--
+	Author: Rohit Menon
+	Matric No. U1222512H
+	Lab group: SSP3
+ -->
 <?php
 
-$apples = $oranges = $bananas = 4;
+$apples = $oranges = $bananas = 0;
+$name = $payment = "";
+// Function to read input from file
+function readFromFile(){
+
+	$file = fopen("order.txt", "r+") or exit("Unable to open order.txt file!");
+	$total_records = array( "apples" => 0,
+						"oranges" => 0,
+						"bananas" => 0);
+
+	foreach ($total_records as $fruit => $quantity) {
+		$line = fgets($file);
+		$quantity = explode(":", $line);
+		
+		// Assign corresponding value of quantity to the fruit
+		$total_records[$fruit] = (int)$quantity[1];
+
+		// echo "$<br> fruit = ";
+		// var_dump($fruit);
+		// echo "<br> total_records = ";
+		var_dump($total_records[$fruit]);
+		// var_dump(total_records);
+		// echo "<br>";
+		
+	}
+	fclose($file);
+	return $total_records;	
+}
+
+function writeToFile($total_records){
+
+	$file = fopen("test.txt", "w+");
+
+	foreach ($total_records as $fruit => $quantity) {
+		// printf("\r\nTotal number of %s" .$fruit. " : " .$quantity);
+		fwrite($file, "Total number of " .$fruit. " : " .$quantity. "\r\n");
+	}
+
+	fclose($file);
+}
+
+
+
+// $test = readFromFile();
+// for ($x=0, ) {
+// 	# code...
+// 	printf($test[$key]);
+// }
+
+// readFromFile();
+// var_dump($test);
+
+// writeToFile($test);
+
+function addQuantities($apples, $oranges, $bananas){
+
+	// Get values from file
+	$temp = readFromFile();
+
+	echo "<br>";
+	var_dump($temp);
+
+	// echo "<br> apples before";
+	// echo $temp["apples"];
+
+	$temp["apples"] += $apples;
+	$temp["oranges"] += $oranges;
+	$temp["bananas"] += $bananas;
+
+	// echo "<br> apples after";
+	// echo $temp["apples"];
+
+	writeToFile($temp);
+}
+
 
 
 if(isset($_POST['Submit'])) {	//POST is not null
 	// var_dump($_POST['Submit']);
+	$name = $_POST['custName'];
+	echo "Payment: ";
+	print($_POST['payment']);
+	if($_POST['payment'] == "visa"){
+		$payment = "Visa";
+	}
+
+	if($_POST['payment'] == "mastercard"){
+		$payment = "Mastercard";
+	}
+
+	if($_POST['payment'] == "discover"){
+		$payment = "Discover";
+	}
+
+
+	echo "<br>";
+	var_dump($name);
 
 	if( (isset($_POST['apples'])) && (isset($_POST['oranges'])) && (isset($_POST['bananas'])) ) {
 		$apples = $_POST['apples'];
 		$oranges = $_POST['oranges'];
 		$bananas = $_POST['bananas'];
 		printf("apples: %u, oranges: %u, bananas: %u", $apples, $oranges, $bananas);
-		$total = ($apples * 0.69) + ($oranges * 0.59) + ($bananas * 0.39);
-	// header("Location: invoice.html");
+		$total = number_format((($apples * 0.69) + ($oranges * 0.59) + ($bananas * 0.39)), 2 );
 		printf("\r\n Total Cost = ");
 		printf($total);
+		
+		echo "<br> <br>";
+		addQuantities($apples, $oranges, $bananas);
+
+		printf("Payment = %s" ,$payment);
+
+		header("Location: invoice.php?".
+				"name=$name&".
+				"apples=$apples&".
+				"oranges=$oranges&".
+				"bananas=$bananas&".
+				"payment=$payment&".
+				"total=$total");
 		exit;
 
 	}
